9a08b08dc2f82a475ce44381eb3caa039e664b5d
hoho00 <kihoyoung@Kis-MacBook-Air.local>
2021-11-03 15:17:53 +0900

    fix: stash

--- lib/common/functions/JungGanBoPlayer.dart
+++ lib/common/functions/JungGanBoPlayer.dart
@@ -4,129 +4,149 @@ import 'package:flutter_midi/flutter_midi.dart';
 import 'dart:async';
 
 void playJungGanBo(JungGanBo jungGanBo) {
-  int i = 1;
-  //jungGanBo.jangDan
-  playJung(jungGanBo.sheet[0]);
-  interval(new Duration(milliseconds: 900), (timer) {
-    if (i < jungGanBo.sheet.length) {
-      playJung(jungGanBo.sheet[i]);
-      i++;
-    } else {
-      timer.cancel();
-      return;
-    }
-  });
+    int i = 1;
+    //jungGanBo.jangDan
+    var prev = playJung(
+        jungGanBo.sheet[0],
+        YulmyeongNote(Yulmyeong.start, ScaleStatus.origin)
+    );
+    interval(new Duration(milliseconds : 900), (timer) {
+        if(i < jungGanBo.sheet.length) {
+            prev = playJung(jungGanBo.sheet[i], prev);
+            i++;
+        } else {
+            timer.cancel();
+            return;
+        }
+    });
 }
 
-playJung(
-  Jung jung,
-) {
-  if (jung.divisionStatus == DivisionStatus.one) {
-    playOneYulmyeongNote(jung.yulmyeongs[0], 900);
-  } else if (jung.divisionStatus == DivisionStatus.two) {
-    playOneYulmyeongNote(jung.yulmyeongs[0], 450);
-    int i = 1;
-    interval(new Duration(milliseconds: 450), (timer) {
-      if (i < 2) {
-        playOneYulmyeongNote(jung.yulmyeongs[i], 450);
-        i++;
-      } else {
-        timer.cancel();
-        return;
-      }
-    });
-  } else if (jung.divisionStatus == DivisionStatus.three) {
-    playOneYulmyeongNote(jung.yulmyeongs[0], 300);
-    int i = 1;
-    interval(new Duration(milliseconds: 300), (timer) {
-      if (i < 3) {
-        playOneYulmyeongNote(jung.yulmyeongs[i], 300);
-        i++;
-      } else {
-        timer.cancel();
-        return;
-      }
-    });
-  }
+YulmyeongNote playJung(Jung jung, YulmyeongNote prev) {
+    if (jung.yulmyeongs[0].yulmyeong == Yulmyeong.long) {} else {
+        if (jung.divisionStatus == DivisionStatus.one) {
+            playOneYulmyeongNote(jung.yulmyeongs[0], 900);
+            return jung.yulmyeongs[0];
+        } else if (jung.divisionStatus == DivisionStatus.two) {
+            playOneYulmyeongNote(jung.yulmyeongs[0], 450);
+            int i = 1;
+            interval(new Duration(milliseconds : 450), (timer) {
+                if(i < 2) {
+                    playOneYulmyeongNote(jung.yulmyeongs[i], 450);
+                    i++;
+                } else {
+                    timer.cancel();
+                    return jung.yulmyeongs[1];
+                }
+            });
+            return jung.yulmyeongs[1];
+        } else if (jung.divisionStatus == DivisionStatus.three) {
+            playOneYulmyeongNote(jung.yulmyeongs[0], 300);
+            int i = 1;
+            interval(new Duration(milliseconds : 300), (timer) {
+                if(i < 3) {
+                    playOneYulmyeongNote(jung.yulmyeongs[i], 300);
+                    i++;
+                } else {
+                    timer.cancel();
+                    return jung.yulmyeongs[2];
+                }
+            });
+            return jung.yulmyeongs[2];
+        }
+    }
+
 }
 
 playOneYulmyeongNote(YulmyeongNote yulmyeongNote, int millisec) {
-  final player = FlutterMidi();
-  int notePlayed = getMidiNoteFromYulmyeongNote(yulmyeongNote);
-  player.playMidiNote(midi: notePlayed);
-  Timer(new Duration(milliseconds: millisec - 10), () {
-    player.stopMidiNote(midi: notePlayed);
-  });
+    final player = FlutterMidi();
+    int notePlayed = getMidiNoteFromYulmyeongNote(yulmyeongNote);
+    player.playMidiNote(midi : notePlayed);
+    Timer(new Duration(milliseconds : millisec - 10), () {
+        player.stopMidiNote(midi : notePlayed);
+    });
 }
 
-makePlaySheet() {}
+YulmyeongNote playOneYulmyeonNoteWithPrev(
+    YulmyeongNote yulmyeongNote,
+    int millisec,
+    YulmyeongNote prevYulmyeongNote
+) {
+    final player = FlutterMidi();
+    int notePlayed = getMidiNoteFromYulmyeongNote(yulmyeongNote);
+    int prevPlayed = getMidiNoteFromYulmyeongNote(prevYulmyeongNote);
+    if (yulmyeongNote.yulmyeong != Yulmyeong.long) {
+        Timer(new Duration(milliseconds : millisec - 10), () {
+            player.stopMidiNote(midi : prevPlayed);
+        });
+    }
+    player.playMidiNote(midi : notePlayed);
+    return yulmyeongNote;
+}
 
 int getMidiNoteFromYulmyeongNote(YulmyeongNote yulmyeongNote) {
-  int res = 0;
-  if (yulmyeongNote.scaleStatus == ScaleStatus.origin) {
-    switch (yulmyeongNote.yulmyeong) {
-      case Yulmyeong.joong:
-        res = JOONG_NOTE;
-        break;
-      case Yulmyeong.yim:
-        res = YIM_NOTE;
-        break;
-      case Yulmyeong.moo:
-        res = MOO_NOTE;
-        break;
-      case Yulmyeong.hwang:
-        res = HWANG_NOTE;
-        break;
-      case Yulmyeong.tae:
-        res = TAE_NOTE;
-        break;
-      case Yulmyeong.blank:
-        res = REST_NOTE;
-        break;
-      case Yulmyeong.rest:
-        res = REST_NOTE;
-        break;
-      default:
+    int res = 0;
+    if (yulmyeongNote.scaleStatus == ScaleStatus.origin) {
+        switch (yulmyeongNote.yulmyeong) {
+            case Yulmyeong.joong:
+                res = JOONG_NOTE;
+                break;
+            case Yulmyeong.yim:
+                res = YIM_NOTE;
+                break;
+            case Yulmyeong.moo:
+                res = MOO_NOTE;
+                break;
+            case Yulmyeong.hwang:
+                res = HWANG_NOTE;
+                break;
+            case Yulmyeong.tae:
+                res = TAE_NOTE;
+                break;
+            case Yulmyeong.blank:
+                res = REST_NOTE;
+                break;
+            case Yulmyeong.rest:
+                res = REST_NOTE;
+                break;
+            default ://high:
+        }
+    } else {
+        switch (yulmyeongNote.yulmyeong) {
+            case Yulmyeong.joong:
+                res = JOONG_HIGH_NOTE;
+                break;
+            case Yulmyeong.yim:
+                res = YIM_HIGH_NOTE;
+                break;
+            case Yulmyeong.moo:
+                res = MOO_HIGH_NOTE;
+                break;
+            case Yulmyeong.hwang:
+                res = HWANG_HIGH_NOTE;
+                break;
+            case Yulmyeong.tae:
+                res = TAE_HIGH_NOTE;
+                break;
+            case Yulmyeong.blank:
+                res = REST_NOTE;
+                break;
+            case Yulmyeong.rest:
+                res = REST_NOTE;
+                break;
+            default:
+        }
     }
-  }
-  //high
-  else {
-    switch (yulmyeongNote.yulmyeong) {
-      case Yulmyeong.joong:
-        res = JOONG_HIGH_NOTE;
-        break;
-      case Yulmyeong.yim:
-        res = YIM_HIGH_NOTE;
-        break;
-      case Yulmyeong.moo:
-        res = MOO_HIGH_NOTE;
-        break;
-      case Yulmyeong.hwang:
-        res = HWANG_HIGH_NOTE;
-        break;
-      case Yulmyeong.tae:
-        res = TAE_HIGH_NOTE;
-        break;
-      case Yulmyeong.blank:
-        res = REST_NOTE;
-        break;
-      case Yulmyeong.rest:
-        res = REST_NOTE;
-        break;
-      default:
-    }
-  }
-  return res;
+    return res;
 }
 
 Timer interval(Duration duration, func) {
-  Timer function() {
-    Timer timer = new Timer(duration, function);
+    Timer function () {
+        Timer timer = new Timer(duration, function);
 
-    func(timer);
+        func(timer);
 
-    return timer;
-  }
+        return timer;
+    }
 
-  return new Timer(duration, function);
+    return new Timer(duration, function);
 }
--- lib/model/jung-gan-bo_model/JungGanBo.dart
+++ lib/model/jung-gan-bo_model/JungGanBo.dart
@@ -1,80 +1,75 @@
 import 'package:danso_function/danso_function.dart';
 
-// 정간보를 읽는 순서는 위에서 아래로, 오른쪽 줄에서 왼쪽 줄로 읽어나간다.
-// 다만 정간 속의 율명은 왼쪽에서 오른쪽으로 읽은 다음 위에서 아래로 읽는다.
-// 정간보에 쓰이는 용어와 중요한 부호는 다음과 같다.
-
-// 각 : 오선보의 한 단에 해당하는 것을 ‘각’ 이라 한다.
-// 강 : 오선보의 한마디에 해당하는 것을 ‘강’ 이라 한다.
-// 정 : 오선보의 한 박에 해당하는 것을 ‘정’ 이라 한다.
+// 정간보를 읽는 순서는 위에서 아래로, 오른쪽 줄에서 왼쪽 줄로 읽어나간다. 다만 정간 속의 율명은 왼쪽에서 오른쪽으로 읽은 다음 위에서
+// 아래로 읽는다. 정간보에 쓰이는 용어와 중요한 부호는 다음과 같다. 각 : 오선보의 한 단에 해당하는 것을 ‘각’ 이라 한다. 강 :
+// 오선보의 한마디에 해당하는 것을 ‘강’ 이라 한다. 정 : 오선보의 한 박에 해당하는 것을 ‘정’ 이라 한다.
 
 class JungGanBo implements JungGanBoInterface {
-  String _title;
-  JangDan _jangDan;
-  List<Jung> _sheet = [];
+    String _title;
+    JangDan _jangDan;
+    List<Jung> _sheet = [];
 
-  String get title => _title;
-  JangDan get jangDan => _jangDan;
-  List<Jung> get sheet => _sheet;
-  //todo
-  //make method
+    String get title => _title;
+    JangDan get jangDan => _jangDan;
+    List<Jung> get sheet => _sheet;
+    //todo make method
 
-  JungGanBo(String title, String jangDan, String formatString) {
-    _title = title;
-    setJangDanString(jangDan);
-    setJungGanBoSheetFromFormatString(formatString);
-  }
-
-  void setJungGanBoSheetFromFormatString(String formatString) {
-    String tmp = "";
-    for (var i = 0; i < formatString.length; i++) {
-      if (formatString[i] == "|") {
-        print(tmp);
-        _sheet.add(new Jung(tmp));
-        tmp = "";
-      } else if (formatString[i] == "#") {
-        print(tmp);
-        _sheet.add(new Jung(tmp));
-        tmp = "";
-      } else {
-        tmp += formatString[i];
-      }
+    JungGanBo(String title, String jangDan, String formatString) {
+        _title = title;
+        setJangDanString(jangDan);
+        setJungGanBoSheetFromFormatString(formatString);
     }
-  }
 
-  //set jangDang(JangDan jangDan) => _jangDan = jangDan;
-  set title(String title) => _title = title;
-
-  void setJangDanString(String jangDan) {
-    JangDanType tmp;
-    switch (jangDan) {
-      case "세마치장단":
-        tmp = JangDanType.semachi;
-        break;
-
-      case "중중모리장단":
-        tmp = JangDanType.joongJoongMori;
-        break;
-
-      case "자진모리장단":
-        tmp = JangDanType.jajinMori;
-        break;
-
-      case "휘모리장단":
-        tmp = JangDanType.huiMori;
-        break;
-
-      case "굿거리장단":
-        tmp = JangDanType.goodGeori;
-        break;
-
-      case "4박장단":
-        tmp = JangDanType.fourByFour;
-        break;
-
-      default:
-        break;
+    void setJungGanBoSheetFromFormatString(String formatString) {
+        String tmp = "";
+        for (var i = 0; i < formatString.length; i++) {
+            if (formatString[i] == "|") {
+                print(tmp);
+                _sheet.add(new Jung(tmp));
+                tmp = "";
+            } else if (formatString[i] == "#") {
+                print(tmp);
+                _sheet.add(new Jung(tmp));
+                tmp = "";
+            } else {
+                tmp += formatString[i];
+            }
+        }
     }
-    _jangDan = JangDan(tmp);
-  }
+
+    //set jangDang(JangDan jangDan) => _jangDan = jangDan;
+    set title(String title) => _title = title;
+
+    void setJangDanString(String jangDan) {
+        JangDanType tmp;
+        switch (jangDan) {
+            case "세마치장단":
+                tmp = JangDanType.semachi;
+                break;
+
+            case "중중모리장단":
+                tmp = JangDanType.joongJoongMori;
+                break;
+
+            case "자진모리장단":
+                tmp = JangDanType.jajinMori;
+                break;
+
+            case "휘모리장단":
+                tmp = JangDanType.huiMori;
+                break;
+
+            case "굿거리장단":
+                tmp = JangDanType.goodGeori;
+                break;
+
+            case "4박장단":
+                tmp = JangDanType.fourByFour;
+                break;
+
+            default:
+                break;
+        }
+        _jangDan = JangDan(tmp);
+    }
 }
